e883764fddd79d00e2d00caf6101cef6699277f8
Emre Eren <emreeren@gmail.com>
2018-05-26 17:01:09 +0300

    default rules

--- src/DataBuilder/Rules/defaultRules.ts
+++ src/DataBuilder/Rules/defaultRules.ts
@@ -67,17 +67,63 @@ const data = {
             r: Result;
             s: State;
             a: Action a.type == 'EXECUTE_COMMAND' && a.data.name == 'Add Product' from s.action;
-            p: Object from s.load('Products',a.params.Name);
+            p: Card from s.load('Products',a.params.Name);
         }
         then {
-            r.add('CREATE_CARD',{type:'Order'});
-            r.add('SET_CARD_TAG',{
-              tag:      'add-product',
-              type:     'Order Product',
-              value:    p.name
+            let portions = p.getSubCard('Portions');
+            if(!portions) {
+              r.add('CREATE_CARD',{type:'Order'});
+              r.add('SET_CARD_TAG',{
+                tag:      'add-product',
+                type:     'Order Product',
+                value:    p.name
+              });
+            }else{
+              r.add('EXECUTE_COMMAND', {name:'Select Portion', params:{Name:p.name}});
+            }
+        }
+    }
+    rule SelectPortion {
+        when {
+            r: Result;
+            s: State;
+            a: Action a.type == 'EXECUTE_COMMAND' && a.data.name == 'Select Portion' from s.action;
+            p: Card from s.load('Products', a.params.Name);
+            portions: Card from p.getSubCard('Portions');
+        }
+        then {
+           r.add('ASK_QUESTION',{
+              question:'Select Portion',
+              tag:'SelectPortion',
+              cardName: a.params.Name,
+              parameters: {
+                'portion': portions.allTags.filter(t=>t.name!=='Name').map(p => p.value + ' (' + p.amount.toFixed(2) + ')=' + p.value)
+              }
             });
         }
     }
+    
+    rule AddProductWithPortion{
+        when {
+          r: Result;
+          s: State;
+          a: Action a.type == 'ASK_QUESTION' && a.data.tag == 'SelectPortion' from s.action;
+          p: Card from s.load('Products', a.data.cardName);
+          portions: Card from p.getSubCard('Portions');
+          selected: String from s.get('portion');
+        }
+        then {
+          let portion = portions.tags.find(t => t.value === selected);
+          r.add('CREATE_CARD',{type:'Order'});
+          r.add('SET_CARD_TAG',{
+            tag     : 'add-product',
+            type    : 'Order Product',
+            value   : p.name,
+            amount  : portion.amount,
+            unit    : portion.value
+          });
+        }
+      }
     `
 };
 
